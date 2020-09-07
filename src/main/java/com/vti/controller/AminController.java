86b49fb39111e66d3f827fe5b99fb984dad18c65
moly7x <16718453+moly7x@users.noreply.github.com>
2020-09-07 22:58:25 +0700

    update cossorigin

--- src/main/java/com/vti/controller/AminController.java
+++ src/main/java/com/vti/controller/AminController.java
@@ -4,6 +4,7 @@ package com.vti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.vti.services.TeamService;
 
 @RestController
 @RequestMapping("api/admin/v1")
+@CrossOrigin(origins = "*")
 public class AminController {
 
 	@Autowired
