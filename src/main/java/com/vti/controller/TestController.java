0aabb2acc641cb1d38ed6dfd63a0e2415bf8ed2c
moly7x <16718453+moly7x@users.noreply.github.com>
2020-09-18 17:00:20 +0700

    file

--- src/main/java/com/vti/controller/TestController.java
+++ src/main/java/com/vti/controller/TestController.java
@@ -1,18 +1,24 @@
 //
 package com.vti.controller;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.vti.dto.ITeamStudentDTO;
 import com.vti.repository.StudentRepo;
 import com.vti.services.StudentService;
-import com.vti.services.TeamService;
 import com.vti.services.TeamStudentService;
 
 @RestController
@@ -20,8 +26,6 @@ import com.vti.services.TeamStudentService;
 @CrossOrigin(origins = "*")
 public class TestController {
 	
-	@Autowired
-	private TeamService teamService;
 	
 	@Autowired
 	private StudentService studentService;
@@ -37,8 +41,28 @@ public class TestController {
 //		System.out.println(request.get("search"));
 //	}
 
-	@GetMapping("test")
-	public List<ITeamStudentDTO> findAllStudent() {
-		return teamStudentService.findAllTeamStudent();
+	@PostMapping("test")
+	public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+		
+		System.out.println(file.getOriginalFilename());
+		
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		
+		Path path = Paths.get(System.getProperty("user.dir"), "scr", "fileCSV");
+		
+		System.out.println(file.getSize());
+
+		
+		try {
+			Files.copy(file.getInputStream(), path.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+//				.path("/files/download/")
+//				.path(fileName)
+//				.toUriString();
+//		return ResponseEntity.ok(fileDownloadUri);
+		return null;
 	}
 }
